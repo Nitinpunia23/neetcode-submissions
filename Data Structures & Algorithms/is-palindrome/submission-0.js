2426373491990d364a52cb55b7146ad709d78451
NITIN PUNIA <85982257+Nitinpunia23@users.noreply.github.com>
2026-07-21 15:04:28 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length -1;
+        while(left < right){
+            while(left <right && !isAlphanumeric(s[left])){
+                left++;
+            }
+            while (left <right && !isAlphanumeric(s[right])){
+                right--;
+            }
+            if(s[left].toLowerCase()!==s[right].toLowerCase()){
+                return false
+            }
+            left++;
+            right--;
+        }
+        return true
+    }
+
+}
+   function isAlphanumeric(ch){
+        return /^[a-z0-9]$/i.test(ch);
+    }
