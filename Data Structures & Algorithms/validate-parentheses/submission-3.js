f4543f7a5e45d80a905cb35c03f7677d62609a66
NITIN PUNIA <85982257+Nitinpunia23@users.noreply.github.com>
2026-07-24 14:55:37 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const stack = [];
+        const map = new Map([
+            ['(',')'],
+            ['[',']'],
+            ['{','}']
+        ])
+        for(let ch of s){
+            if(map.has(ch)){
+                stack.push(ch);
+            }else{
+                if(stack.length===0){
+                    return false;
+                }
+                const top = stack.pop();
+                if(map.get(top) !==ch){
+                    return false
+                }
+            }
+        }
+        return stack.length == 0;
+    }
+}
